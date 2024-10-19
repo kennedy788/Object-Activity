d77c2545e1ff90b393e55fe16bbfe76f3d340959
kennedy788 <salvadorjohnkennedy78@gmail.com>
2024-10-19 21:34:05 +0800

    Create objectActivity.js

---
+++ objectActivity.js
@@ -0,0 +1,11 @@
+let car = { 
+  type: "Luxury Sports Car",
+  model: "Lamborghini",
+  color: "Black"
+};
+
+console.log(typeof car);
+
+car.type = "Luxury Sports Car";
+
+console.log(car); // Output: { type: 'Ford', model: 'Mustang', color: 'Black' } 
